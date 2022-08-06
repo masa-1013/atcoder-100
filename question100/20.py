62ea62359b8e64e93118ba33f7150a7e52ef2098
masahiro <wktkmrnhrtmmshr@gmail.com>
2022-08-06 13:38:45 +0900

    20.py

--- question100/20.py
+++ question100/20.py
@@ -7,17 +7,13 @@ a_parts_list_len = len(a_parts_list)
 b_parts_list_len = len(b_parts_list)
 c_parts_list_len = len(c_parts_list)
 
-def search_up(parts, parts_list, left, right):
-  if (left == right): return left
-  if (parts_list[0] > parts): return 0
-  if (parts_list[-1] < parts): return -1
+def search_left(parts, parts_list, left, right):
+  if (parts_list[0] >= parts): return 0
+  if (parts_list[-1] < parts): return right + 1
 
   while (right > left):
-    if (parts_list[right] == parts): return right
-    if (parts_list[left] == parts): return left
-
     if (right - left == 1):
-      if parts_list[left] == parts:
+      if parts_list[left] >= parts:
         return left
       elif parts_list[right] >= parts:
         return right
@@ -26,25 +22,21 @@ def search_up(parts, parts_list, left, right):
     
     index = (left + right) // 2
 
-    if (parts >= parts_list[index]):
+    if (parts > parts_list[index]):
       left = index
     else:
       right = index
 
-def search_down(parts, parts_list, left, right):
-  if (left == right): return left
-  if (parts_list[0] > parts): return -1
-  if (parts_list[-1] < parts): return right
+def search_right(parts, parts_list, left, right):
+  if (parts_list[0] > parts): return 0
+  if (parts_list[-1] <= parts): return right + 1
 
   while (right > left):
-    if (parts_list[right] == parts): return right
-    if (parts_list[left] == parts): return left
-
     if (right - left == 1):
-      if parts_list[right] == parts:
-        return right
+      if parts_list[right] <= parts:
+        return right + 1
       elif parts_list[left] <= parts:
-        return left
+        return right
       else:
         return -1
     
@@ -58,17 +50,9 @@ def search_down(parts, parts_list, left, right):
 ans = 0
 
 for b_parts in b_parts_list:
-  a_parts_index = search_down(b_parts, a_parts_list, 0, a_parts_list_len - 1)
-  if (a_parts_list[a_parts_index] == b_parts):
-    a_parts_index -= 1
-  if (a_parts_index < 0): continue
+  a_parts_index = search_left(b_parts, a_parts_list, 0, a_parts_list_len - 1)
+  c_parts_index = search_right(b_parts, c_parts_list, 0, c_parts_list_len - 1)
 
-  c_parts_index = search_up(b_parts, c_parts_list, 0, c_parts_list_len - 1)
-  if (c_parts_index == -1): continue
-  if (c_parts_list[c_parts_index] == b_parts):
-    c_parts_index += 1
-
-  total = (a_parts_index + 1) * (c_parts_list_len - c_parts_index)
-  ans += total
+  ans += (a_parts_index * (c_parts_list_len - c_parts_index))
 
 print(ans)
--- sample.py
+++ sample.py
@@ -1,5 +1,5 @@
 from bisect import bisect_left, bisect_right
 
 a = [1,10,20,30]
-print(bisect_left(a, 5))
+print(bisect_left(a, 0))
 print(bisect_right(a, 5))
